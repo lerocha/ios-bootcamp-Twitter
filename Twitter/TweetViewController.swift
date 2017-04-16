bdc3b7f317358a71f40e4cd8c19f8076beceff36
Luis Rocha <lerocha@gmail.com>
2017-04-16 16:24:16 -0700

    Initial setup for Tweet controller

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Luis Rocha on 4/16/17.
+//  Copyright © 2017 Luis Rocha. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func onReplyButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
